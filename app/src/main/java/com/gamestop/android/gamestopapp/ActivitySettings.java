e0720661238e7de8556a7bd978ea07a1bb1d322f
Baldi00 <you@example.com>
2019-05-19 13:28:32 +0200

    ActivitySettings bug when rotate, reset

--- app/src/main/java/com/gamestop/android/gamestopapp/ActivitySettings.java
+++ app/src/main/java/com/gamestop/android/gamestopapp/ActivitySettings.java
@@ -198,12 +198,7 @@ public class ActivitySettings extends AppCompatActivity {
                             NotificationManager nManager = ((NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE));
                             nManager.cancelAll();
 
-                            Intent i = getBaseContext().getPackageManager().
-                                    getLaunchIntentForPackage(getBaseContext().getPackageName());
-                            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                            finish();
-                            startActivity(i);
+                            finishAndRemoveTask();
 
                         } catch (Exception e) {
                             e.printStackTrace();
